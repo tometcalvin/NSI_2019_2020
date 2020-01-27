add7d8d218ec916611ddac6170732b685bfd27ef
tometcalvin <40426010+tometcalvin@users.noreply.github.com>
2020-01-27 12:50:45 +0100

    Create quizz.js

---
+++ quizz.js
@@ -0,0 +1,19 @@
+function choixqcm1(document) {
+  if (document.qcm1.choix[3].checked) {
+    alert("Bonne réponse")
+  }
+  else {
+    alert("Réponse fausse")
+  }
+
+};
+
+function choixqcm2(document) {
+  if (document.qcm2.choix[2].checked) {
+    alert("Bonne réponse")
+  }
+  else {
+    alert("Réponse fausse")
+  }
+
+};
